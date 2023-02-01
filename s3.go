dba3c697229939b5e92f4dc744ad4578d548181f
Dalcio <dalciosan@gmail.com>
2023-02-01 12:54:49 +0100

    logs

--- s3.go
+++ s3.go
@@ -135,7 +135,7 @@ func (s S3Service) UploadAsMultipart(file *bytes.Buffer, path string) (string, e
 	index := 0
 	for bytesRead < lengthFile {
 		index += 1
-		fmt.Println("index", index)
+		fmt.Println("index, bytesRead, lengthFile", index, bytesRead, lengthFile)
 		bytesRead += copy(buffer, buffer[bytesRead:])
 		// Upload a part
 		result, err := s3.New(s.connectToS3()).UploadPart(&s3.UploadPartInput{
